c16e5b4f1407e084231f4413a89e0ab7ad92b21d
Amisha Verma <92872778+amysaa@users.noreply.github.com>
2023-07-10 12:09:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// document.getElementById("count-el").innerText=5
+// let count = 0;
+// count = count +1;
+// console.log(count)
+let saveEl= document.getElementById("save-el");
+
+
+ let countEl =document.getElementById("count-el");
+ let count = 0;
+//  console.log(countEl);
+
+ console.log(saveEl);
+
+
+function increment(){
+    count += 1;
+    countEl.innerText= count;
+}
+
+function save(){
+    let countStr = count + " - " ;
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+
+
+// console.log(count);
+
+}
